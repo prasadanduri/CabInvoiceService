06c5ceb8e4b82073b6157fc9aa1aade590249fef
prasad <prasadanduri13gmail.com>
2022-04-09 14:45:43 +0530

    Cab invoice generator

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Gernerator");
+
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+
+            Console.WriteLine(fare);
+        }
+    }
+}
